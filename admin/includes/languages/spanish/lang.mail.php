f3fbcf06c2f6ef32c1ea868272eb1369881ffa3c
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2024-01-01 18:31:34 +0200

    New translations lang.mail.php (spanish)

---
+++ admin/includes/languages/spanish/lang.mail.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @copyright Copyright 2003-2022 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: DrByte 2020 Dec 25 New in v1.5.8-alpha $
+*/
+
+$define = [
+    'HEADING_TITLE' => 'Enviar Emails a los Clientes',
+    'TEXT_CUSTOMER' => 'Cliente:',
+    'TEXT_SUBJECT' => 'Asunto:',
+    'TEXT_FROM' => 'De:',
+    'TEXT_MESSAGE' => 'Mensaje <br>sólo texto:',
+    'TEXT_MESSAGE_HTML' => 'Mensaje <br>RTF:',
+    'TEXT_ATTACHMENTS_LIST' => 'Adjunto seleccionado: ',
+    'TEXT_SELECT_ATTACHMENT' => 'Adjunto<br>en servidor: ',
+    'TEXT_SELECT_ATTACHMENT_TO_UPLOAD' => 'Adjunto<br>a subir<br>y adjuntar: ',
+    'TEXT_ATTACHMENTS_DIR' => 'Directorio para subida: ',
+    'NOTICE_EMAIL_SENT_TO' => 'Aviso: Email enviado a: %s',
+    'NOTICE_EMAIL_FAILED_SEND' => 'Aviso: FALLO en el intento enviar Email a todos los destinatarios: %s',
+    'ERROR_NO_CUSTOMER_SELECTED' => 'Error: No ha seleccionado ningún cliente.',
+    'ERROR_NO_SUBJECT' => 'Error: No se ha introducido ningún asunto.',
+    'ERROR_ATTACHMENTS' => 'Error: No puede seleccionar SUBIR y AÑADIR a la vez adjuntos separados. Seleccione sólo uno => por favor.',
+];
+
+return $define;
