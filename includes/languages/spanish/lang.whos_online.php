8c20a3a2b384e0d0144dacb2bc4590d2b26f16f4
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2024-01-01 18:33:25 +0200

    New translations lang.whos_online.php (spanish)

---
+++ includes/languages/spanish/lang.whos_online.php
@@ -0,0 +1,14 @@
+<?php
+$define = [
+    'BOX_HEADING_WHOS_ONLINE' => 'Usuarios Conectados',
+    'BOX_WHOS_ONLINE_THEREIS' => 'Actualmente hay',
+    'BOX_WHOS_ONLINE_THEREARE' => 'Actualmente hay',
+    'BOX_WHOS_ONLINE_GUEST' => 'visitante',
+    'BOX_WHOS_ONLINE_GUESTS' => 'visitantes',
+    'BOX_WHOS_ONLINE_AND' => 'y',
+    'BOX_WHOS_ONLINE_MEMBER' => 'cliente',
+    'BOX_WHOS_ONLINE_MEMBERS' => 'clientes',
+    'BOX_WHOS_ONLINE_ONLINE' => 'conectado(s).',
+];
+
+return $define;
