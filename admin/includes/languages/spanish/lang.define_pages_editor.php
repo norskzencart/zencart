d2f8e5c17af7b71689ff5fa5c3e9eff5c5346c25
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2024-01-01 18:31:18 +0200

    New translations lang.define_pages_editor.php (spanish)

---
+++ admin/includes/languages/spanish/lang.define_pages_editor.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @copyright Copyright 2003-2022 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: Zcwilt 2020 Jun 02 New in v1.5.8-alpha $
+*/
+
+$define = [
+    'HEADING_TITLE' => 'Editor de Define Pages para: ',
+    'TEXT_FILE_DOES_NOT_EXIST' => 'No existe el archivo: %s',
+    'ERROR_FILE_NOT_WRITEABLE' => 'Error: No puede escribirse este archivo. Revise el permiso de usuario en: %s',
+    'TEXT_INFO_SELECT_FILE' => 'Seleccione un archivo a editar ...',
+    'TEXT_INFO_EDITING' => 'Editando archivo:',
+    'TEXT_INFO_CAUTION' => 'Nota: Debe editar siempre los archivos localizados en el directorio de su plantilla actual. Ejemplo: /languages/' . '/html_includes/YOUR_TEMPLATE_DIRECTORY<br>Asegúrese de hacer copias de seguridad antes de modificar sus archivos.' . '/html_includes/YOUR_TEMPLATE_DIRECTORY<br>Be sure to make backups after changing your files.',
+];
+
+return $define;
