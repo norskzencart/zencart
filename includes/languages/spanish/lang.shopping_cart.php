ac38d6e91ce2bc227a0d6f12a16fe11be6585581
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2024-01-01 18:33:20 +0200

    New translations lang.shopping_cart.php (spanish)

---
+++ includes/languages/spanish/lang.shopping_cart.php
@@ -0,0 +1,13 @@
+<?php
+$define = [
+    'TEXT_INFORMATION' => 'You may proceed with your purchase by clicking the Checkout button below. Shipping and Taxes and Discounts will be handled on subsequent pages.',
+    'NAVBAR_TITLE' => 'Carro de la Compra',
+    'HEADING_TITLE' => 'Su carro de la compra contiene:',
+    'TABLE_HEADING_REMOVE' => 'Quitar',
+    'TABLE_HEADING_PRICE' => 'Precio',
+    'TEXT_CART_EMPTY' => 'Su carro de la compra está vacío.',
+    'SUB_TITLE_SUB_TOTAL' => 'Subtotal:',
+    'TEXT_CART_HELP' => '<a href="javascript:session_win();">[ayuda (?)]</a>',
+];
+
+return $define;
