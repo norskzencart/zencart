a8564f8087fcec4b2febeb156ac63cca1fcee5fa
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2024-01-01 18:31:16 +0200

    New translations lang.customer_groups.php (spanish)

---
+++ admin/includes/languages/spanish/lang.customer_groups.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @copyright Copyright 2003-2022 Zen Cart Development Team
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: Scott C Wilson 2022 Jun 04 New in v1.5.8-alpha $
+*/
+$define = [
+    'HEADING_TITLE' => 'Grupos de Clientes',
+    'TABLE_HEADING_GROUP_NAME' => 'Nombre Grupo',
+    'TABLE_HEADING_GROUP_CUSTOMER_COUNT' => '# Clientes',
+    'TABLE_HEADING_GROUP_COMMENTS' => 'Descripción',
+    'TEXT_HEADING_ADD_GROUP' => 'Añadir Grupo',
+    'TEXT_HEADING_EDIT_GROUP' => 'Editar Grupo',
+    'TEXT_HEADING_DELETE_GROUP' => 'Borrar Grupo',
+    'TEXT_NO_GROUPS_FOUND' => 'No hay ningún Grupo. Pinche en Insertar para crear uno.',
+    'TEXT_NEW_INTRO' => 'Describe el Grupo nuevo',
+    'TEXT_DELETE_INTRO' => '¿Está seguro que quiere borrar este Grupo?',
+    'TEXT_DELETE_EVEN_IF_CUSTOMERS_ASSIGNED' => '¿Borrar aunque hay clientes asignados al Grupo?',
+    'TEXT_DELETE_WARNING_GROUP_MEMBERS_EXIST' => '¡<b>PRECAUCIÓN:</b> Hay %s clientes asignados a este Grupo!',
+    'TEXT_GROUP_NAME' => 'Nombre Grupo:',
+    'TEXT_GROUP_COMMENT' => 'Descripción:',
+    'TEXT_CUSTOMERS_IN_GROUP' => 'Clientes en Grupo:',
+    'ERROR_GROUP_STILL_HAS_CUSTOMERS' => 'ERROR: Hay clientes asignados a este Grupo. Confirme que quiere quitar los clientes de este Grupo y borrarlo.',
+];
+
+return $define;
