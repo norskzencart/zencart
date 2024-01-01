316d513510d0dd983a6c3bc1b7be8fc6868dd4b0
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2024-01-01 18:31:48 +0200

    New translations lang.paypal.php (spanish)

---
+++ admin/includes/languages/spanish/lang.paypal.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @copyright Copyright 2003-2022 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: Scott C Wilson 2022 Jan 09 New in v1.5.8-alpha $
+*/
+
+$define = [
+    'TEXT_PAYPAL_IPN_SORT_ORDER_INFO' => 'Mostrar Pedido: ',
+    'TEXT_SORT_PAYPAL_ID_DESC' => 'Pedido PayPal Recibido (nuevo - antiguo)',
+    'TEXT_SORT_PAYPAL_ID' => 'PayPal Pedido Recibido (nuevo - antiguo)',
+    'TEXT_SORT_ZEN_ORDER_ID_DESC' => 'ID Pedido (alto - bajo) => PayPal Pedido Recibido',
+    'TEXT_SORT_ZEN_ORDER_ID' => 'Pedido (bajo - alto) => PayPal Pedido Recibido',
+    'TEXT_PAYMENT_AMOUNT_DESC' => 'Valor del Pedido (alto - bajo)',
+    'TEXT_PAYMENT_AMOUNT' => 'Valor del Pedido (bajo - alto)',
+    'HEADING_ADMIN_TITLE' => 'Notificaciones de pago instantáneo de PayPal',
+    'HEADING_PAYMENT_STATUS' => 'Estado del pago',
+    'TEXT_ALL_IPNS' => 'Todo',
+    'TABLE_HEADING_ORDER_NUMBER' => 'Pedido Nº',
+    'TABLE_HEADING_PAYPAL_ID' => 'PayPal #',
+    'TABLE_HEADING_TXN_TYPE' => 'Tipo de transacción',
+    'TABLE_HEADING_PAYMENT_STATUS' => 'Estado del pago',
+    'TABLE_HEADING_PAYMENT_AMOUNT' => 'Cantidad',
+    'TABLE_HEADING_NUM_HISTORY_ENTRIES' => 'Cantidad entradas en el historial de estado',
+    'TABLE_HEADING_ENTRY_NUM' => 'Entrada número',
+    'TABLE_HEADING_TRANS_ID' => 'ID de trans.',
+    'TABLE_HEADING_PENDING_REASON' => 'Razón Pendiente',
+    'TEXT_INFO_PAYPAL_IPN_HEADING' => 'PayPal IPN',
+    'TEXT_DISPLAY_PAYPAL_IPN_NUMBER_OF_TX' => 'Mostrando del <strong>%d</strong> al <strong>%d</strong> (de <strong>%d</strong> Transacciones)',
+];
+
+return $define;
