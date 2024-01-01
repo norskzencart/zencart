0838a2ef44415a1353246b0351170fe68060f999
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2024-01-01 18:32:41 +0200

    New translations lang.contact_us.php (spanish)

---
+++ includes/languages/spanish/lang.contact_us.php
@@ -0,0 +1,9 @@
+<?php
+$define = [
+    'HEADING_TITLE' => 'Contáctenos',
+    'NAVBAR_TITLE' => 'Contáctenos',
+    'TEXT_SUCCESS' => 'Su mensaje ha sido enviado correctamente.',
+    'EMAIL_SUBJECT' => 'Mensaje de la página de contacto de ' . STORE_NAME,
+];
+
+return $define;
