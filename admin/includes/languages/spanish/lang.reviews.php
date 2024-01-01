f8806cbed5f6196b3aea70d90aab84ed41eecb55
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2024-01-01 18:31:57 +0200

    New translations lang.reviews.php (spanish)

---
+++ admin/includes/languages/spanish/lang.reviews.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @copyright Copyright 2003-2022 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: Scott C Wilson 2022 Jan 09 New in v1.5.8-alpha $
+*/
+
+$define = [
+    'HEADING_TITLE' => 'Comentarios',
+    'TABLE_HEADING_CUSTOMER_NAME' => 'Cliente',
+    'TABLE_HEADING_LANGUAGE' => 'Idioma',
+    'TABLE_HEADING_RATING' => 'Puntuación',
+    'ENTRY_PRODUCT' => 'Producto:',
+    'ENTRY_FROM' => 'De:',
+    'ENTRY_DATE' => 'Fecha:',
+    'ENTRY_REVIEW' => 'Comentario:',
+    'ENTRY_REVIEW_TEXT' => '<b>NOTA:</b> ¡No se admite los tags HTML!',
+    'ENTRY_RATING' => 'Puntuación:',
+    'TEXT_INFO_DELETE_REVIEW_INTRO' => '¿Seguro que desea eliminar este comentario?',
+    'TEXT_INFO_REVIEW_AUTHOR' => 'Autor:',
+    'TEXT_INFO_REVIEW_RATING' => 'Puntuación:',
+    'TEXT_INFO_REVIEW_READ' => 'Leer:',
+    'TEXT_INFO_REVIEW_SIZE' => 'Tamaño:',
+    'TEXT_INFO_PRODUCTS_AVERAGE_RATING' => 'Puntuación media:',
+    'TEXT_OF_5_STARS' => '¡%s de 5 Estrellas!',
+    'TEXT_GOOD' => 'BUENO',
+    'TEXT_BAD' => 'MALO',
+    'TEXT_INFO_HEADING_DELETE_REVIEW' => 'Borrar Comentario',
+    'TEXT_EDIT_REVIEW' => 'Editar',
+    'TEXT_DELETE_REVIEW' => 'Borrar',
+    'TEXT_VIEW_REVIEW' => 'Ver (Catálogo)',
+    'TEXT_EDIT_PRODUCT' => 'Editar Producto',
+    'TEXT_ALL_STATUS' => '--Cualquiera--',
+    'TEXT_PENDING_APPROVAL' => 'Pendiente de Aprobar',
+    'TEXT_APPROVED' => 'Aprobado',
+    'HEADING_TITLE_STATUS' => 'Estado',
+];
+
+return $define;
