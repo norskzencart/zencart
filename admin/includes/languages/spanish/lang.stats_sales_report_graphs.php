13e598cb573091d0cff2bc23bcade7ddb80c1d9c
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2024-01-01 18:32:07 +0200

    New translations lang.stats_sales_report_graphs.php (spanish)

---
+++ admin/includes/languages/spanish/lang.stats_sales_report_graphs.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @copyright Copyright 2003-2022 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: Zcwilt 2020 Jun 02 New in v1.5.8-alpha $
+*/
+
+$define = [
+    'FILTER_STATUS' => '<b>Filtrar por Estado del Pedido </b>',
+    'FILTER_VALUE' => '<b>Filtro Activo/Inactivo </b>',
+    'HEADING_TITLE' => 'Informes de Ventas',
+    'TODAY_TO_DATE' => 'Totales Diarios',
+    'WEEK_TO_DATE' => 'Totales Semanales',
+    'MONTH_TO_DATE' => 'Totales Mensuales',
+    'AVERAGE_ORDER' => 'Valor Promedio de Pedido',
+    'CHART_TOTAL_SALES' => 'Total de Ventas',
+    'CHART_AVERAGE_SALE_AMOUNT' => 'Venta Promedia',
+    'TEXT_PREVIOUS_LINK' => '&lt;&lt;&nbsp;Previa',
+    'TEXT_NEXT_LINK' => 'Próxima&nbsp;&gt;&gt;',
+    'REPORT_TEXT_ORDERS' => 'Pedidos',
+    'REPORT_TEXT_CONVERSION_PER_ORDER' => 'Conversión por Pedido',
+    'REPORT_TEXT_CONVERSION' => 'Conversión',
+    'REPORT_TEXT_VARIANCE' => 'Variante',
+    'REPORT_TEXT_HOURLY' => 'Por Hora',
+    'REPORT_TEXT_DAILY' => 'Diario',
+    'REPORT_TEXT_WEEKLY' => 'Semanal',
+    'REPORT_TEXT_MONTHLY' => 'Mensual',
+    'REPORT_TEXT_YEARLY' => 'Anual',
+    'YEARLY_TOTAL' => 'Total Anual',
+    'CHART_TEXT_AVERAGE' => 'Promedio',
+];
+
+return $define;
