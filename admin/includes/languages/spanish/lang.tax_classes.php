7e8422c0832dcb43ef3a3dac7cf24a654da14ba7
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2024-01-01 18:32:09 +0200

    New translations lang.tax_classes.php (spanish)

---
+++ admin/includes/languages/spanish/lang.tax_classes.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @copyright Copyright 2003-2022 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: Scott C Wilson 2022 Jan 09 New in v1.5.8-alpha $
+*/
+
+$define = [
+    'HEADING_TITLE' => 'Clases de Impuestos',
+    'TABLE_HEADING_TAX_CLASS_ID' => 'ID',
+    'TABLE_HEADING_TAX_CLASSES' => 'Impuesto',
+    'TEXT_INFO_CLASS_TITLE' => 'Impuesto:',
+    'TEXT_INFO_CLASS_DESCRIPTION' => 'Descripción:',
+    'TEXT_INFO_INSERT_INTRO' => 'Introduzca el nuevo impuesto con sus datos relacionados',
+    'TEXT_INFO_DELETE_INTRO' => '¿Seguro que desea eliminar este impuesto?',
+    'TEXT_INFO_HEADING_NEW_TAX_CLASS' => 'Nuevo impuesto',
+    'TEXT_INFO_HEADING_EDIT_TAX_CLASS' => 'Editar Impuesto',
+    'TEXT_INFO_HEADING_DELETE_TAX_CLASS' => 'Eliminar Impuesto',
+    'ERROR_TAX_RATE_EXISTS_FOR_CLASS' => 'ERROR: No puede borrar esta Clase de Impuestos -- Hay Tasas de Impuestos vinculadas a esta clase.',
+    'ERROR_TAX_RATE_EXISTS_FOR_PRODUCTS' => 'ERROR: No puede borrar esta Clase de Impuestos -- Hay %s productos vinculados a esta clase.',
+];
+
+return $define;
