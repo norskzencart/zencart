22ac04d76ee2544db2efb78166d44268f080e6ec
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2024-01-01 18:31:13 +0200

    New translations lang.coupon_admin_export.php (spanish)

---
+++ admin/includes/languages/spanish/lang.coupon_admin_export.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @copyright Copyright 2003-2022 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: Scott C Wilson 2022 Jan 11 New in v1.5.8-alpha $
+*/
+
+$define = [
+    'HEADING_TITLE' => 'Exportar Cupones',
+    'HEADING_SUB1' => 'Revisar o Exportar Cupones',
+    'HEADING_SUB2' => 'Purgar Historial',
+    'TEXT_ACTIVITY_EXPORT_FORMAT' => 'Formato del Archivo del Exportación:',
+    'TEXT_ACTIVITY_EXPORT_FILENAME' => 'Nombre del Archivo de Exportación:',
+    'TEXT_ACTIVITY_EXPORT_SAVETOFILE' => '¿Guardar el archivo en el servidor? (si no => se lo descargará desde el navegador)',
+    'TEXT_ACTIVITY_EXPORT_DEST' => 'Destino: ',
+    'SUCCESS_EXPORT_DISCOUNT_COUPON_LOG' => 'Exportación completado. ',
+    'FAILURE_EXPORT_DISCOUNT_COUPON_LOG' => 'AVISO: La exportación falló. No se pudo escribir al archivo.',
+    'TEXT_INSTRUCTIONS' => '<u>INSTRUCTIONS</u><br>Exportar los Cupones de Descuento a un archivo CSV.<br>
+<ol><li>Elija mostrar o exportar a un archivo.<li>Introduzca un nombre de archivo (debe tener uno de estos sufijos: .csv .txt .htm .html .xml)<li>Pinche Guardad para seguir.<li>Elija  guardar o abrir el archivo => dependiente de las opciones de este navegador.</ol>',
+];
+
+return $define;
