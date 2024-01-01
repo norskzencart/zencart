badb3668d6256c9f491a59c9c36e8078ee707d43
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2024-01-01 18:33:44 +0200

    New translations define_shopping_cart.txt (spanish)

---
+++ includes/languages/spanish/html_includes/define_shopping_cart.php
@@ -0,0 +1,7 @@
+<p>
+    El contenido de su carrito de compras se muestra a continuación. Puede actualizar cantidades o eliminar artículos haciendo clic en los botones relacionados.
+    
+    If relevant, Shipping, Taxes and Discounts will be determined and displayed during Checkout.
+    
+    ¡Por supuesto, puede agregar más artículos a su carrito!
+</p>
