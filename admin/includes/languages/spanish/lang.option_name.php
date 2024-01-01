a2545e98be40348f33ed5af9bc1bc8ab0b50352f
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2024-01-01 18:31:40 +0200

    New translations lang.option_name.php (spanish)

---
+++ admin/includes/languages/spanish/lang.option_name.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @copyright Copyright 2003-2022 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: Scott C Wilson 2022 Jan 11 New in v1.5.8-alpha $
+*/
+
+$define = [
+    'HEADING_TITLE' => 'Órdenes de las Opciones',
+    'TEXT_EDIT_ALL' => 'Editando todos los Nombres de las Opciones',
+    'TEXT_CURRENT_NAME' => 'Nombre Actual',
+    'TEXT_OPTION_TYPE' => 'Tipo',
+    'TEXT_SELECTED_LANGUAGE' => 'Idioma: ',
+    'TEXT_UPDATE_SUBMIT' => 'Actualizar Órden',
+    'SUCCESS_OPTION_SORT_ORDER' => 'Los órdenes de los Nombres de las Opciones han sido actualizados correctamente',
+];
+
+return $define;
