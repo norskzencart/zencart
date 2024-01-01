3d51e7397fcd71ea6c22156ba59a036ceffb89ed
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2024-01-01 18:32:40 +0200

    New translations lang.conditions.php (spanish)

---
+++ includes/languages/spanish/lang.conditions.php
@@ -0,0 +1,8 @@
+<?php
+$define = [
+    'NAVBAR_TITLE' => 'Condiciones de Uso',
+    'HEADING_TITLE' => 'Condiciones de Uso',
+    'TEXT_INFORMATION' => 'Información sobre las Condiciones de Uso debe estar en esta página',
+];
+
+return $define;
