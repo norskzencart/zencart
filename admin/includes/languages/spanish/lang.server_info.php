13ba612c246104be04565ff1548ad6e646ee13a3
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2024-01-01 18:32:00 +0200

    New translations lang.server_info.php (spanish)

---
+++ admin/includes/languages/spanish/lang.server_info.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @copyright Copyright 2003-2022 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: torvista 2022 Jun 30 New in v1.5.8-alpha $
+*/
+
+$define = [
+    'HEADING_TITLE' => 'Información del Servidor',
+    'TITLE_SERVER_HOST' => 'Servidor:',
+    'TITLE_SERVER_OS' => 'SO del Servidor:',
+    'TITLE_SERVER_DATE' => 'Fecha del Servidor:',
+    'TITLE_SERVER_UP_TIME' => 'Tiempo de Actividad del Servidor:',
+    'TITLE_HTTP_SERVER' => 'Servidor HTTP:',
+    'TITLE_PHP_VERSION' => 'Versión PHP:',
+    'TITLE_ZEND_VERSION' => 'Zend:',
+    'TITLE_DATABASE_HOST' => 'Servidor de la BBDD:',
+    'TITLE_DATABASE' => 'Base de Datos:',
+    'TITLE_DATABASE_DATE' => 'Fecha de la BBDD:',
+    'TITLE_DATABASE_DATA_SIZE' => 'Tamaño de la BBDD:',
+    'TITLE_DATABASE_INDEX_SIZE' => 'Tamaño del Índice de la BBDD:',
+    'TITLE_PHP_MEMORY_LIMIT' => 'PHP Limité de Memoria:',
+    'TITLE_PHP_FILE_UPLOADS' => 'PHP Subida de Archivos:',
+    'TITLE_PHP_UPLOAD_MAX' => 'Tamaño Máximo de un Archivo de Subida:',
+    'TITLE_PHP_POST_MAX_SIZE' => 'POST Tamaño Máximo:',
+    'TITLE_DATABASE_ENGINE' => 'Engine Base de Datos: ',
+    'PROJECT_DATABASE_LABEL' => 'Nivel de actualización de la base de datos: ',
+    'TITLE_MYSQL_STRICT_MODE' => '(en modo Strict)',
+    'TITLE_DATABASE_MYSQL_MODE' => 'Modo MySQL:',
+    'TITLE_DATABASE_MYSQL_SLOW_LOG_STATUS' => 'MySQL Slow Query Estatus Historial:',
+    'TITLE_DATABASE_MYSQL_SLOW_LOG_FILE' => 'MySQL Slow Query Historial:',
+    'TEXT_DATABASE_VARIABLES_LINK' => '<a href="#db-h2">Ver los variables del base de datos</a>',
+    'TITLE_DATABASE_VARIABLES' => 'Variables del Base de Datos: ',
+    'HEADING_DATABASE_VARIABLE' => 'Nombre Variable',
+    'HEADING_DATABASE_VALUE' => 'Valor',
+];
+
+return $define;
