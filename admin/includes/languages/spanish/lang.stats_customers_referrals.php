2ffc07b77256a1d38693192f438ed463a2e01246
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2024-01-01 18:32:04 +0200

    New translations lang.stats_customers_referrals.php (spanish)

---
+++ admin/includes/languages/spanish/lang.stats_customers_referrals.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @copyright Copyright 2003-2022 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: Zcwilt 2020 Jun 02 New in v1.5.8-alpha $
+*/
+
+$define = [
+    'HEADING_TITLE' => 'Informe de Origenes de clientes',
+    'TEXT_INFO_SELECT_REFERRAL' => 'Seleccione una Referencia/Código de Cupón',
+    'TEXT_REFERRAL_UNKNOWN' => 'Desconocido',
+    'TEXT_INFO_START_DATE' => 'Fecha Inicio (m-d-y)',
+    'TEXT_INFO_END_DATE' => 'Fecha Final (m-d-y)',
+    'TEXT_ORDER_NUMBER' => 'Pedido #',
+    'TEXT_COUPON_ID' => 'Cupón de Descuento ID#',
+];
+
+return $define;
