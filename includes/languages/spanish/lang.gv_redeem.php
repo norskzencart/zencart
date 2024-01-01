1735effc15f9581169cc5c1ae7dfaf3b9239f6d6
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2024-01-01 18:32:53 +0200

    New translations lang.gv_redeem.php (spanish)

---
+++ includes/languages/spanish/lang.gv_redeem.php
@@ -0,0 +1,11 @@
+<?php
+$define = [
+    'NAVBAR_TITLE' => 'Canjear ' . TEXT_GV_NAME,
+    'HEADING_TITLE' => 'Canjear ' . TEXT_GV_NAME,
+    'TEXT_INFORMATION' => 'Para más información sobre los ' . TEXT_GV_NAME . ', por favor, vea nuestro <a href="' . zen_href_link(FILENAME_GV_FAQ) . '">' . GV_FAQ . '.</a>',
+    'TEXT_INVALID_GV' => 'El código del ' . TEXT_GV_NAME . ' number %s may be invalid or has already been redeemed. To contact the shop owner please use the Contact Page.',
+    'TEXT_VALID_GV' => 'Enhorabuena, ha canjeado un ' . TEXT_GV_NAME . ' que vale %s.',
+    'ERROR_GV_CREATE_ACCOUNT' => 'To redeem a Gift Voucher you must create an account.',
+];
+
+return $define;
