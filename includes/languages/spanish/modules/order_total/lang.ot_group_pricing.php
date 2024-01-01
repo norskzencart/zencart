0dbc7a373ea4104b0096948f9dd5466e69baec07
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2024-01-01 18:34:11 +0200

    New translations lang.ot_group_pricing.php (spanish)

---
+++ includes/languages/spanish/modules/order_total/lang.ot_group_pricing.php
@@ -0,0 +1,7 @@
+<?php
+$define = [
+    'MODULE_ORDER_TOTAL_GROUP_PRICING_TITLE' => 'Descuento por Grupo',
+    'MODULE_ORDER_TOTAL_GROUP_PRICING_DESCRIPTION' => 'Descuento por Grupo',
+];
+
+return $define;
