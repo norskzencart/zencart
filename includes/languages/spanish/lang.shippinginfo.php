d3c77c9508ee66014b60d6f5d7dcc94ab84d2d11
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2024-01-01 18:33:19 +0200

    New translations lang.shippinginfo.php (spanish)

---
+++ includes/languages/spanish/lang.shippinginfo.php
@@ -0,0 +1,8 @@
+<?php
+$define = [
+    'NAVBAR_TITLE' => 'Envíos y Devoluciones',
+    'HEADING_TITLE' => 'Envíos y Devoluciones',
+    'TEXT_INFORMATION' => 'Su política de Envíos y Devoluciones debería ir aquí.',
+];
+
+return $define;
