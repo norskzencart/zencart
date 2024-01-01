91f7bb68fdcb17ad55776daa9dca7b349ea28b27
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2024-01-01 18:34:26 +0200

    New translations lang.flat.php (spanish)

---
+++ includes/languages/spanish/modules/shipping/lang.flat.php
@@ -0,0 +1,8 @@
+<?php
+$define = [
+    'MODULE_SHIPPING_FLAT_TEXT_TITLE' => 'Tarifa Plana',
+    'MODULE_SHIPPING_FLAT_TEXT_DESCRIPTION' => 'Tarifa Plana',
+    'MODULE_SHIPPING_FLAT_TEXT_WAY' => 'Mensajería',
+];
+
+return $define;
