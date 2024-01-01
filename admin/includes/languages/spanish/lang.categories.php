126db2e2504c82e88b626be73155865d7a97d93f
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2024-01-01 18:31:09 +0200

    New translations lang.categories.php (spanish)

---
+++ admin/includes/languages/spanish/lang.categories.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @copyright Copyright 2003-2022 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: Scott C Wilson 2022 Jan 09 New in v1.5.8-alpha $
+*/
+
+$define = [
+    'HEADING_TITLE' => 'Categorías / Productos',
+    'TEXT_EDIT_CATEGORIES_NAME' => 'Nombre de la Categoría:',
+    'TEXT_CATEGORIES_DESCRIPTION' => 'Descripción de la Categoría:',
+    'TEXT_CATEGORIES_IMAGE' => 'Imagen de la Categoría:',
+    'TEXT_FILENAME' => 'Archivo: %1$s (%2$upx x %3$upx / %4$ukB)',
+    'TEXT_EDIT_CATEGORIES_IMAGE' => 'Cambiar Imagen:',
+    'TEXT_CATEGORIES_IMAGE_DIR' => 'Subir al directorio / Utilice una imagen existente en:<br>/images/<em>(subdirectory)</em>',
+    'TEXT_CATEGORIES_IMAGE_MANUAL' => 'O, => seleccione una imagen de la carpeta seleccionada => nombre del archivo:',
+    'TEXT_EDIT_SORT_ORDER' => 'Orden:',
+    'TEXT_RESTRICT_PRODUCT_TYPE' => 'Restringir a un Product Type',
+    'TEXT_CATEGORY_HAS_RESTRICTIONS' => 'Esta categoría ha sido restringida a estos Product Types',
+    'MESSAGE_IMAGE_REMOVED_CATEGORY' => 'Categoría #%1$u "%2$s": imagen borrada',
+    'MESSAGE_IMAGE_ADDED_MANUAL' => 'La imagen fue añadida manualmente a la categoría #%1$u "%2$s".',
+    'ERROR_IMAGE_MANUAL_NOT_FOUND' => 'No se encontró la imagen "%s".',
+    'TEXT_INFO_HEADING_EDIT_CATEGORY_META_TAGS' => 'Definiciones Metas Tags para Categorías',
+    'TEXT_EDIT_CATEGORIES_META_TAGS_INTRO' => 'Define Meta Tags Personalizados para ID#%1$u - "%2$s"',
+    'TEXT_EDIT_CATEGORIES_META_TAGS_TITLE' => 'Título Página (pestaña navegador):',
+    'TEXT_EDIT_CATEGORIES_META_TAGS_KEYWORDS' => 'Meta Palabras Claves:',
+    'TEXT_EDIT_CATEGORIES_META_TAGS_DESCRIPTION' => 'Meta Descripción:',
+];
+
+return $define;
