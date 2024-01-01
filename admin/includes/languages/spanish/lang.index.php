765d53bd1af5ae0dea7912750e9403b6607a3f8d
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2024-01-01 18:31:30 +0200

    New translations lang.index.php (spanish)

---
+++ admin/includes/languages/spanish/lang.index.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @copyright Copyright 2003-2022 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: Scott C Wilson 2022 Jan 11 New in v1.5.8-alpha $
+*/
+
+$define = [
+    'HEADING_TITLE' => 'Elija una acción...',
+    'BOX_TITLE_ORDERS' => 'Estados de los Pedidos',
+    'BOX_TITLE_STATISTICS' => 'Estadísticas',
+    'BOX_TITLE_FEATURES_SALES' => 'Ofertas / Destacados / Rebajas',
+    'WO_GRAPH_TITLE' => 'Quien está Online:',
+    'WO_GRAPH_MORE' => 'mas...',
+    'WO_GRAPH_REGISTERED' => 'Usuario:',
+    'WO_GRAPH_GUEST' => 'Invitado:',
+    'WO_GRAPH_SPIDER' => 'Araña:',
+    'WO_GRAPH_TOTAL' => 'Total:',
+    'DASHBOARD_DAY' => 'Día',
+    'DASHBOARD_SESSIONS' => 'Sesiones',
+    'DASHBOARD_TOTAL' => 'Total',
+    'DASHBOARD_MONTH' => 'Mes',
+    'DASHBOARD_SALES' => 'Ventas',
+    'TEXT_COUNTER_HISTORY_TITLE' => 'Visitor History - Last %s days',
+    'TEXT_MONTHLY_SALES_TITLE' => 'Ventas Mensuales (excluyendo envíos)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;',
+    'TEXT_CLICK_FOR_COMPLETE_DETAILS' => 'Pinche aquí para ver los detalles...',
+    'TEXT_SALES_TODAY' => 'Ventas <strong>Hoy</strong> (%s) &nbsp;<strong>$%s</strong>',
+    'TEXT_SALES_YESTERDAY' => '<strong>Ayer</strong> (%s) &nbsp;<strong>$%s</strong>',
+    'BOX_ENTRY_CUSTOMERS' => 'Clientes:',
+    'BOX_ENTRY_NEWSLETTERS' => 'Suscritores al Boletín:',
+    'BOX_ENTRY_PRODUCTS' => 'Productos:',
+    'BOX_ENTRY_PRODUCTS_OFF' => 'Productos desactivados:',
+    'BOX_ENTRY_REVIEWS' => 'Comentarios:',
+    'BOX_ENTRY_REVIEWS_PENDING' => 'Comentarios pendientes de aprobar:',
+    'BOX_ENTRY_NEW_CUSTOMERS' => 'Nuevos clientes:',
+    'BOX_ENTRY_NEW_ORDERS' => 'Nuevos pedidos:',
+    'BOX_ENTRY_SPECIALS_EXPIRED' => 'Ofertas Caducados',
+    'BOX_ENTRY_FEATURED_EXPIRED' => 'Productos Destacados caducados',
+    'BOX_ENTRY_SALEMAKER_EXPIRED' => 'Rebajas Caducadas',
+    'BOX_ENTRY_SPECIALS_ACTIVE' => 'Ofertas Activos',
+    'BOX_ENTRY_FEATURED_ACTIVE' => 'Productos Destacados Activos',
+    'BOX_ENTRY_SALEMAKER_ACTIVE' => 'Rebajas Activas',
+    'SESSION' => 'Sesión',
+    'TOTAL' => 'Total',
+    'TEXT_UPDATE' => 'Actualizar',
+    'TEXT_STORE_NAME' => 'Nombre de la Tienda',
+    'TEXT_STORE_OWNER' => 'Propietario de la Tienda',
+    'TEXT_STORE_OWNER_EMAIL' => 'Email del Propietario',
+    'TEXT_STORE_COUNTRY' => 'País de la Tienda',
+    'TEXT_STORE_ZONE' => 'Zona de la Tienda',
+    'TEXT_STORE_ADDRESS' => 'Dirección de la Tienda',
+    'HEADING_TITLE_WIZARD' => 'Asistente de Configuración Inicial',
+    'TEXT_STORE_DETAILS' => 'Introduzca los datos de la Tienda. Todos los campos son obligatorios',
+];
+
+return $define;
