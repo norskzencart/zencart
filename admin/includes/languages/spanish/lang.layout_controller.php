3b4072e7532942f4c6f232e7aabe84a3041d50a3
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2024-01-01 18:31:32 +0200

    New translations lang.layout_controller.php (spanish)

---
+++ admin/includes/languages/spanish/lang.layout_controller.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @copyright Copyright 2003-2022 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: Scott C Wilson 2022 Jan 11 New in v1.5.8-alpha $
+*/
+
+$define = [
+    'HEADING_TITLE' => 'Recuadros de las Columnas',
+    'TEXT_CURRENTLY_VIEWING' => 'Actualmente Mostrando: ',
+    'TEXT_THIS_IS_PRIMARY_TEMPLATE' => ' (Principal)',
+    'TABLE_HEADING_LAYOUT_BOX_NAME' => 'Nombre del archivo del recuadro',
+    'TABLE_HEADING_LAYOUT_BOX_STATUS' => 'Columna Izq/Dch<br>ESTATUS',
+    'TABLE_HEADING_LAYOUT_BOX_STATUS_SINGLE' => 'Columna Única<br>ESTATUS',
+    'TABLE_HEADING_LAYOUT_BOX_LOCATION' => 'Columna<br>IZQ/DCH',
+    'TABLE_HEADING_LAYOUT_BOX_SORT_ORDER' => 'Columna Izq/Dch<br>ORDEN',
+    'TABLE_HEADING_LAYOUT_BOX_SORT_ORDER_SINGLE' => 'Columna Única<br>ORDEN',
+    'TEXT_INFO_LAYOUT_BOX' => 'Recuadro Seleccionado: ',
+    'TEXT_INFO_LAYOUT_BOX_NAME' => 'Nombre Recuadro:',
+    'TEXT_INFO_LAYOUT_BOX_LOCATION' => 'Ubicación: ',
+    'TEXT_INFO_LAYOUT_BOX_STATUS' => 'Estado de Columna Izquierda/Derecha: ',
+    'TEXT_INFO_LAYOUT_BOX_STATUS_SINGLE' => 'Estado de Columna Única: ',
+    'TEXT_INFO_LAYOUT_BOX_SORT_ORDER' => 'Orden de Columna Izquierda/Derecha:',
+    'TEXT_INFO_LAYOUT_BOX_SORT_ORDER_SINGLE' => 'Orden de Columna Única',
+    'TEXT_INFO_INSERT_INTRO' => 'Introduzca los datos del recuadro nuevo',
+    'TEXT_INFO_DELETE_INTRO' => '¿Seguro que desea eliminar este recuadro?',
+    'TEXT_INFO_HEADING_EDIT_BOX' => 'Editar Recuadro',
+    'TEXT_INFO_HEADING_DELETE_BOX' => 'Eliminar Recuadro',
+    'TEXT_INFO_DELETE_MISSING_LAYOUT_BOX' => 'Eliminar recuadro faltante del listado de esta plantilla: ',
+    'TEXT_INFO_DELETE_MISSING_LAYOUT_BOX_NOTE' => 'NOTA: Esto no elimina ficheros así que puede volver a añadir el cuadro en cualquier momento sólo añadiéndolo al directorio correcto.<br><br><strong>Eliminar nombre de recuadro:</strong>',
+    'TEXT_INFO_RESET_TEMPLATE_SORT_ORDER' => 'Resetear los estados de los recuadros: ',
+    'TEXT_INFO_RESET_TEMPLATE_SORT_ORDER_NOTE' => 'Esto no elimina ninguno de los recuadros. Solo restablecerá los estados y orden de los recuadros iguales a la otra plantilla.',
+    'TEXT_SETTINGS_COPY_FROM' => 'Copiar estados y orden DESDE: ',
+    'TEXT_SETTINGS_COPY_TO' => ' A: ',
+    'TEXT_ERROR_INVALID_RESET_SUBMISSION' => 'ERROR: elección inválida',
+    'TEXT_INFO_BOX_DETAILS' => 'Detalles de Recuadro: ',
+    'TABLE_HEADING_LAYOUT_TITLE' => 'Título',
+    'TABLE_HEADING_LAYOUT_VALUE' => 'Valor',
+    'TABLE_HEADING_BOXES_PATH' => 'Ruta Recuadros: ',
+    'TEXT_WARNING_NEW_BOXES_FOUND' => 'AVISO: Se ha encontrado Recuadros nuevos: ',
+    'TEXT_MODULE_DIRECTORY' => 'Directorio de la disposición del sitio:',
+    'TEXT_GOOD_BOX' => ' ',
+    'TEXT_BAD_BOX' => '<span class="txt-red"><b>FALTA</b></span><br>',
+    'SUCCESS_BOX_DELETED' => 'Borrado correctamente el recuadro: ',
+    'SUCCESS_BOX_RESET' => 'La configuración para [%s] ha sido cambiado a la configuración de [%s].',
+    'SUCCESS_BOX_UPDATED' => 'Recuadro actualizada correctamente: ',
+    'TEXT_ON' => ' ACTIVADO ',
+    'TEXT_OFF' => ' DESACTIVADO ',
+    'TEXT_LEFT' => ' IZQ ',
+    'TEXT_RIGHT' => ' DCH ',
+    'TEXT_CAUTION_EDITING_NOT_LIVE_TEMPLATE' => 'AVISO: No está editando la plantilla actualment en uso en la tienda.',
+    'TEXT_RESET_SETTINGS' => 'Resetear Configuración',
+    'TEXT_ORIGINAL_DEFAULTS' => '[Valores por Defecto Zen Cart original]',
+];
+
+return $define;
